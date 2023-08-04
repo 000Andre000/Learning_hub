11dbe9144f4bf4ca794a5229d6b9c087509f49d3
000Andre000 <a.fernandes1808@gmail.com>
2023-08-05 01:52:32 +0530

    Secondcommit

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
-//jshintesversion:6
+
 
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+
+const app = express(); 
+const port = 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+app.get("/", function(req,res){
+    res.sendFile(__dirname+"/home.html");
+})
+
+app.listen(port,function(){
+    console.log("Server started at port" + port);
+});
+
+// public\img\img1.png
+// public\css\style.css
